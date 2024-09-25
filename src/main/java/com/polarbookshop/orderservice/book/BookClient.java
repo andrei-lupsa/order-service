46b9100aab0e032a17eba78cbfb6912b7c1e14cd
Andrei Lupsa <andrei.lupsa@endava.com>
2024-09-25 17:03:57 +0300

    8.4 Resilience

--- src/main/java/com/polarbookshop/orderservice/book/BookClient.java
+++ src/main/java/com/polarbookshop/orderservice/book/BookClient.java
@@ -1,14 +1,18 @@
 package com.polarbookshop.orderservice.book;
 
+import com.polarbookshop.orderservice.config.ClientProperties;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 import org.springframework.web.reactive.function.client.WebClient;
+import org.springframework.web.reactive.function.client.WebClientResponseException;
 import reactor.core.publisher.Mono;
+import reactor.util.retry.Retry;
 
 @RequiredArgsConstructor
 @Component
 public class BookClient {
     private static final String BOOKS_ROOT_API = "/books/{isbn}";
+    private final ClientProperties clientProperties;
     private final WebClient webClient;
 
     public Mono<Book> getBookByIsbn(String isbn) {
@@ -16,6 +20,12 @@ public class BookClient {
                 .get()
                 .uri(BOOKS_ROOT_API, isbn)
                 .retrieve()
-                .bodyToMono(Book.class);
+                .bodyToMono(Book.class)
+                .timeout(clientProperties.timeout(), Mono.empty())
+                .onErrorResume(WebClientResponseException.NotFound.class, ex -> Mono.empty())
+                .retryWhen(
+                        Retry.backoff(clientProperties.retryBackoffMaxAttempts(), clientProperties.retryMinBackoff())
+                )
+                .onErrorResume(Exception.class, ex -> Mono.empty());
     }
 }
--- src/main/java/com/polarbookshop/orderservice/config/ClientProperties.java
+++ src/main/java/com/polarbookshop/orderservice/config/ClientProperties.java
@@ -4,8 +4,12 @@ import jakarta.validation.constraints.NotNull;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 
 import java.net.URI;
+import java.time.Duration;
 
 @ConfigurationProperties(prefix = "polar")
 public record ClientProperties(
-        @NotNull URI catalogServiceUri) {
+        @NotNull URI catalogServiceUri,
+        @NotNull Duration timeout,
+        @NotNull Long retryBackoffMaxAttempts,
+        @NotNull Duration retryMinBackoff) {
 }
